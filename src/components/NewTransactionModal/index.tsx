d965604912a7a9c503627f855dff4f70bc464f7a
Maiara Lange <maiaralange@microsoft.com>
2022-07-29 12:28:54 -0300

    Styling type buttons in modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImage from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImage from '../../assets/income.svg';
+import outcomeImage from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -29,6 +31,18 @@ export function NewTransactionModal({
         <h2>Cadastrar transação</h2>
         <input placeholder="Título" />
         <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImage} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImage} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
